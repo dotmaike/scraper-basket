711ed58613f42d919bb4d3d7d5cf8960a1534b5e
Miguel Murillo <miguel_murillo@epam.com>
2018-11-20 13:00:33 -0600

    Updating app

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-const cron = require('node-cron');
 const express = require('express');
-const fs = require('fs');
+const PORT = process.env.PORT || 5000;
 const request = require('request');
 const cheerio = require('cheerio');
 const moment = require('moment-timezone');
-const PORT = process.env.PORT || 5000;
-const app = express();
+const mcache = require('memory-cache');
 
 const cache = duration => {
   return (req, res, next) => {
@@ -25,66 +23,61 @@ const cache = duration => {
   };
 };
 
-app.get('/', cache(60), (req, res) => {
-  /* cron.schedule("* * * * *", function() {
-    console.log("running a task every minute");
-  }); */
-  const mainUrl = 'http://www.magnoliga7.com';
-  const url = `${mainUrl}/index.php/calendario-de-juegos`;
-  const json = {
-    calendar: {
-      updated: moment().format(),
-      timeZone: 'America/Mexico_City',
-      items: []
-    }
-  };
-  request(url, function(error, response, html) {
-    if (!error) {
-      let $ = cheerio.load(html);
-      const calendarPath = $(html)
-        .find('.cat-list-row1 > td > a')
-        .attr('href');
-      const calendarUrl = `${mainUrl}${calendarPath}`;
-      request(calendarUrl, function(err, response, DOM) {
-        if (!err) {
-          let $ = cheerio.load(DOM);
-          const tree = $('tr:contains("EPAM")');
-          $(tree).each(function(index, element) {
-            const hour = $(element)
-              .find('td:first-child')
-              .text()
-              .trim();
-            const challenger = $(element)
-              .find('td:nth-child(3)')
-              .text()
-              .split('(')[0]
-              .trim();
-            const location = $(element)
-              .closest('table')
-              .prev()
-              .text()
-              .trim();
-            const dateTime = $(element)
-              .closest('table')
-              .prev()
-              .prev()
-              .text();
-            json.calendar.items.push({
-              status: 'confirmed',
-              htmlLink: calendarUrl,
-              dateTime: `${dateTime} - ${hour}`,
-              summary: 'Next Game',
-              description: `EPAM vs ${challenger}`,
-              location: location
+express()
+  .get('/', cache(60), (req, res) => {
+    const mainUrl = 'http://www.magnoliga7.com';
+    const url = `${mainUrl}/index.php/calendario-de-juegos`;
+    const json = {
+      calendar: {
+        updated: moment().format(),
+        timeZone: 'America/Mexico_City',
+        items: []
+      }
+    };
+    request(url, function(error, response, html) {
+      if (!error) {
+        let $ = cheerio.load(html);
+        const calendarPath = $(html)
+          .find('.cat-list-row1 > td > a')
+          .attr('href');
+        const calendarUrl = `${mainUrl}${calendarPath}`;
+        request(calendarUrl, function(err, response, DOM) {
+          if (!err) {
+            let $ = cheerio.load(DOM);
+            const tree = $('tr:contains("EPAM")');
+            $(tree).each(function(index, element) {
+              const hour = $(element)
+                .find('td:first-child')
+                .text()
+                .trim();
+              const challenger = $(element)
+                .find('td:nth-child(3)')
+                .text()
+                .split('(')[0]
+                .trim();
+              const location = $(element)
+                .closest('table')
+                .prev()
+                .text()
+                .trim();
+              const dateTime = $(element)
+                .closest('table')
+                .prev()
+                .prev()
+                .text();
+              json.calendar.items.push({
+                status: 'confirmed',
+                htmlLink: calendarUrl,
+                dateTime: `${dateTime} - ${hour}`,
+                summary: 'Next Game',
+                description: `EPAM vs ${challenger}`,
+                location: location
+              });
             });
-          });
-        }
-        res.send(json);
-      });
-    }
-  });
-});
-
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-exports = module.exports = app;
+          }
+          res.send(json);
+        });
+      }
+    });
+  })
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
